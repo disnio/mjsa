8911fb4f94b7785684501731903eb860442799e4
disnio <291166364@qq.com>
2015-05-13 18:06:05 +0800

    jquery/插件/jq插件写法.js Signed-off-by: disnio <291166364@qq.com>

--- api/js/jq-ui/jqui.md
+++ api/js/jq-ui/jqui.md
@@ -1,25 +1,36 @@
+## jquery-ui-bootstrap ##
+5/13/2015 4:02:46 PM 
 
-----------------------------------
-jqui-core：
+https://github.com/jackzhaojun/jquery-ui-bootstrap
+
+## jqui-core ##
+
 z-index： ie 返回 0 未指定时，其他返回字符串。ie 中无法区分一个 0 显式值和无值。
 
 .scrollParent() 获取最近的可滚动的祖先。
+
 .zIndex() 为元素获取 z-index。
+
 .disableSelection() 禁用匹配元素集合内的文本内容。
+
 .enableSelection() 启用匹配元素集合内的文本内容。
 
 :data 选择数据已存储在指定的键下的元素。
+
 :focusable 选择可被聚焦的元素。
+
 :tabbable 选择用户可通过 tab 键聚焦的元素。
 
 $.ui.plugin 废弃. 用 $.widget() 代替。
 
 过滤器选择器函数的写法：
+
 $("element:visible") 
 $.expr.filters.visible( element )
 
-扩展选择器：
-$.extend( $.expr[ ":" ], {
+**扩展选择器：**
+
+`$.extend( $.expr[ ":" ], {
     data: $.expr.createPseudo ?
         $.expr.createPseudo(function( dataName ) {
             return function( elem ) {
@@ -30,18 +41,22 @@ $.extend( $.expr[ ":" ], {
         function( elem, i, match ) {
             return !!$.data( elem, match[ 3 ] );
         }         
-})
+})`
 
 p:252 事件绑定
+
 $.support.selectstart = "onselectstart" in document.createElement( "div" );
+
 IE/Safari/Chrome可以用 onselectstart 阻止用户选定元素内文本
---------------------------------------------
-jqui-widget：
+
+## jqui-widget ##
 
 jQuery.widget.bridge( name, constructor ) 由 $.widget() 创建的对象和 jQuery API 之间的中介。
+
 constructor 当插件被调用时要实例化的对象。
 
 作用：
+
     连接一个常规的 js 构造函数到 jQuery API。
     自动创建对象实例，并存储在元素的 $.data 缓存内。
     允许调用公有方法。
@@ -49,20 +64,19 @@ constructor 当插件被调用时要实例化的对象。
     防止在未初始化的对象上调用方法。
     防止多个初始化。
 
-为了使用桥（bridge）的所有特性，您的对象原型需要有一个 _init() 方法。该方法在调用插件且实例已存在时调用。
+为了使用桥（bridge）的所有特性，您的对象原型需要有一个 _init() 方法。
+该方法在调用插件且实例已存在时调用。
 
 您还需要有一个 option() 方法。
--------------
-jQuery.widget( name [, base ], prototype )
-使用相同的抽象化来创建有状态的 jQuery 插件。
+
+jQuery.widget( name [, base ], prototype ) 使用相同的抽象化来创建有状态的 jQuery 插件。
 
 Base Widget 部件库（Widget Factory）使用的基础小部件。
 
-匹配的jq集合（数组）的第一个元素就是当前操作元素
-element = $( element || this.defaultElement || this )[ 0 ];
+匹配的jq集合（数组）的第一个元素就是当前操作元素 element = $( element || this.defaultElement || this )[ 0 ];
 
-选择 ui 组件：
-$(":ui-progressbar")
+选择 ui 组件：$(":ui-progressbar")
 
 http://www.2cto.com/kf/201410/342653.html
 
+$(that.options.filter, that.element[0]);
---
+++ api/js/jquery/src/callbacks.js
@@ -0,0 +1,219 @@
+define([
+	"./core",
+	"./var/rnotwhite"
+], function( jQuery, rnotwhite ) {
+
+// String to Object options format cache
+var optionsCache = {};
+
+// Convert String-formatted options into Object-formatted ones and store in cache
+//字符串格式选项转换成Object-formatted并存储在缓存中
+function createOptions( options ) {
+	var object = optionsCache[ options ] = {};
+	jQuery.each( options.match( rnotwhite ) || [], function( _, flag ) {
+		object[ flag ] = true;
+	});
+	return object;
+}
+
+/*
+ * Create a callback list using the following parameters:
+ *
+ *	options: an optional list of space-separated options that will change how
+ *			the callback list behaves or a more traditional option object
+ *
+ * By default a callback list will act like an event callback list and can be
+ * "fired" multiple times.
+ *
+ * Possible options:
+ *
+ *	once:			will ensure the callback list can only be fired once (like a Deferred)
+ *
+ *	memory:			will keep track of previous values and will call any callback added
+ *					after the list has been fired right away with the latest "memorized"
+ *					values (like a Deferred)
+ *
+ *	unique:			will ensure a callback can only be added once (no duplicate in the list)
+ *
+ *	stopOnFalse:	interrupt callings when a callback returns false
+ *
+ */
+jQuery.Callbacks = function( options ) {
+
+	// Convert options from String-formatted to Object-formatted if needed
+	// (we check in cache first)
+	options = typeof options === "string" ?
+		( optionsCache[ options ] || createOptions( options ) ) :
+		jQuery.extend( {}, options );
+
+	var // Flag to know if list is currently firing
+        //如果列表是当前触发中则标记
+		firing,
+		// Last fire value (for non-forgettable lists)
+        //最后调用fire是传递的值
+		memory,
+		// Flag to know if list was already fired
+        //标记列表是已经触发的
+		fired,
+		// End of the loop when firing
+        //当触发时循环的次数（实际list的长度）
+		firingLength,
+		// Index of currently firing callback (modified by remove if needed)
+        //当前就绪回调指数(如果需要删除之前修改)
+		firingIndex,
+		// First callback to fire (used internally by add and fireWith)
+        //第一次回调就绪(通过添加和fireWith内部使用)
+		firingStart,
+		// Actual callback list
+        //实际回调列表
+		list = [],
+		// Stack of fire calls for repeatable lists
+        //启动堆调用可重复的列表
+		stack = !options.once && [],
+		// Fire callbacks
+		fire = function( data ) {
+			memory = options.memory && data;
+			fired = true;
+			firingIndex = firingStart || 0;
+			firingStart = 0;
+			firingLength = list.length;
+			firing = true;
+			for ( ; list && firingIndex < firingLength; firingIndex++ ) {
+				if ( list[ firingIndex ].apply( data[ 0 ], data[ 1 ] ) === false && options.stopOnFalse ) {
+					memory = false; // To prevent further calls using add//防止进一步调用使用添加
+					break;
+				}
+			}
+			firing = false;
+			if ( list ) {
+				if ( stack ) {
+                    // memory规则
+					if ( stack.length ) {
+						fire( stack.shift() );
+					}
+				} else if ( memory ) {
+                    //如果是memory+once规则 每次触发回调后清空list
+					list = [];
+				} else {
+                    //once 规则
+					self.disable();
+				}
+			}
+		},
+		// Actual Callbacks object
+		self = {
+			// Add a callback or a collection of callbacks to the list
+			add: function() {
+				if ( list ) {
+					// First, we save the current length
+					var start = list.length;
+					(function add( args ) {
+						jQuery.each( args, function( _, arg ) {
+							var type = jQuery.type( arg );
+							if ( type === "function" ) {
+								if ( !options.unique || !self.has( arg ) ) {
+									list.push( arg );
+								}
+							} else if ( arg && arg.length && type !== "string" ) {
+								// Inspect recursively
+                                //递归检测 （如何是数组递归调用）
+								add( arg );
+							}
+						});
+					})( arguments );
+					// Do we need to add the callbacks to the
+					// current firing batch?
+                    //我们需要添加回调到当前触发的一批（多线程的情况下，如果添加一个回调的时候状态还是执行中就改变当前回调list的长度）
+					if ( firing ) {
+						firingLength = list.length;
+					// With memory, if we're not firing then
+					// we should call right away
+					} else if ( memory ) {
+						firingStart = start;
+						fire( memory );
+					}
+				}
+				return this;
+			},
+			// Remove a callback from the list
+			remove: function() {
+				if ( list ) {
+					jQuery.each( arguments, function( _, arg ) {
+						var index;
+						while ( ( index = jQuery.inArray( arg, list, index ) ) > -1 ) {
+							list.splice( index, 1 );
+							// Handle firing indexes
+							if ( firing ) {
+								if ( index <= firingLength ) {
+									firingLength--;
+								}
+								if ( index <= firingIndex ) {
+									firingIndex--;
+								}
+							}
+						}
+					});
+				}
+				return this;
+			},
+			// Check if a given callback is in the list.
+			// If no argument is given, return whether or not list has callbacks attached.
+			has: function( fn ) {
+				return fn ? jQuery.inArray( fn, list ) > -1 : !!( list && list.length );
+			},
+			// Remove all callbacks from the list
+			empty: function() {
+				list = [];
+				firingLength = 0;
+				return this;
+			},
+			// Have the list do nothing anymore
+			disable: function() {
+				list = stack = memory = undefined;
+				return this;
+			},
+			// Is it disabled?
+			disabled: function() {
+				return !list;
+			},
+			// Lock the list in its current state
+			lock: function() {
+				stack = undefined;
+				if ( !memory ) {
+					self.disable();
+				}
+				return this;
+			},
+			// Is it locked?
+			locked: function() {
+				return !stack;
+			},
+			// Call all callbacks with the given context and arguments
+			fireWith: function( context, args ) {
+				if ( list && ( !fired || stack ) ) {
+					args = args || [];
+					args = [ context, args.slice ? args.slice() : args ];
+					if ( firing ) {
+						stack.push( args );
+					} else {
+						fire( args );
+					}
+				}
+				return this;
+			},
+			// Call all the callbacks with the given arguments
+			fire: function() {
+				self.fireWith( this, arguments );
+				return this;
+			},
+			// To know if the callbacks have already been called at least once
+			fired: function() {
+				return !!fired;
+			}
+		};
+
+	return self;
+};
+
+return jQuery;
+});
