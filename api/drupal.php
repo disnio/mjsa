f138a7946b8a2c4080c57b445db03fc7cb4acedb
disnio <291166364@qq.com>
2015-07-28 17:41:28 +0800

    drupal.php
    Signed-off-by: disnio <291166364@qq.com>

---
+++ api/drupal.php
@@ -0,0 +1,8 @@
+主题：
+http://www.ibm.com/developerworks/cn/opensource/os-new-drupal-theme/
+http://blog.csdn.net/wjc19911118/article/details/7752479
+主题 tpl 可用变量：
+https://www.drupal.org/node/190815
+
+在 Drupal 7 中创建自定义 Web 服务项目
+http://www.ibm.com/developerworks/cn/opensource/os-drupal-web-service-project/index.html
