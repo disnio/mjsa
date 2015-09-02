b8c1ad5da11636eb6cc985c4f7c717507bc7a1a4
disnio <291166364@qq.com>
2015-09-02 17:43:44 +0800

    func.js
    Signed-off-by: disnio <291166364@qq.com>

--- api/python/mongoDB.js
+++ api/python/mongoDB.js
@@ -7,3 +7,120 @@ c:\mongo\bin\mongod.exe --dbpath=c:\mongo\data
 c:\Program Files\MongoDB 2.6 Standard\bin>mongod.exe --dbpath=d:\mongo\data --logpath d:\mongo\mongodb.log --install
 
 service mongod start
+-------------------------------------------
+mongod 启动数据库进程
+--dbpath 指定数据库的目录
+--port 指定数据库的端口,默认是 27017
+--bind_ip 绑定 IP
+--directoryperdb 为每个 db 创建一个独立子目录
+--logpath 指定日志存放目录
+--logappend 指定日志生成方式(追加/覆盖)
+--pidfilepath 指定进程文件路径，如果不指定，那么将不产生进程文件
+--keyFile 集群模式的关键标识
+--cpu 周期性的显示 CPU 和 IO 的利用率
+--journal 启用日志
+--ipv6 启用 IPV6 支持
+--nssize 指定.ns 文件的大小，单位 MB，默认是 16M，最大是 2GB
+--maxConns 最大的并发连接数
+--notablescan 不允许进行表扫描
+--quota 限制每个数据库的文件个数，默认是 8 个
+--quotaFiles 每个数据库的文件个数，配合—quota 参数
+--noprealloc 关闭数据文件的预分配功能
+
+关闭：
+> use admin;
+> db.shutdownServer();
+
+MongoDB 包含 string, integer, boolean, double, null, array, and object 基本的数据类型外，
+还包含： date, object id, binary data, regular expression, and code 这些附加的数据类型。
+timestamp 类型的字段必须是位于文档的前两位
+
+关联有两种，一种是简单的手动关联，一种是用 DBRef。
+//查找
+> db.post.save({title:'MongoDB Manual',author:'sam'});
+> p = db.post.findOne();
+{
+    "_id" : ObjectId("4de36b33282677bdc555a83a"),
+    "title" : "MongoDB Manual",
+    "author" : "sam"
+}
+//关联
+> db.authors.findOne({name:p.author});
+{
+    "_id" : ObjectId("4de36c14282677bdc555a83b"),
+    "name" : "sam",
+    "age" : 24,
+    "email" : "sanlai_lee@lisanlai.cn"
+}
+
+DBRef 关联语法：
+{ $ref : <collname>, $id : <idvalue>[, $db : <dbname>] }
+
+> x = { name : 'Biology' }
+> db.courses.save(x)
+> stu = { name : 'Joe', classes : [ new DBRef('courses', x._id) ] }
+// or we could write:
+// stu = { name : 'Joe', classes : [ {$ref:'courses',$id:x._id} ] }
+> db.students.save(stu)
+> stu
+{
+    "name" : "Joe",
+    "classes" : [
+        {
+            "$ref" : "courses",
+            "$id" :  ObjectId("4b0552b0f0da7d1eb6f126a1")
+        }
+    ],
+    "_id" : ObjectId("4b0552e4f0da7d1eb6f126a2")
+}
+> stu.classes[0]
+{ "$ref" : "courses", "$id" : ObjectId("4b0552b0f0da7d1eb6f126a1") }
+> stu.classes[0].fetch()
+{ "_id" : ObjectId("4b0552b0f0da7d1eb6f126a1"), "name" :"Biology" }
+
+--------------------------------------
+建立索引的函数： ensureIndex()
+db.persons.getIndexes();
+db.collection.dropIndexes();
+db.myCollection.reIndex()
+// same as:
+db.runCommand( { reIndex : 'myCollection' } )
+
+--------------------------------------
+Type Name Type Number
+Double 1
+String 2
+Object 3
+Array 4
+Binary data 5
+Object id 7
+Boolean 8
+Date 9
+Null 10
+Regular expression 11
+JavaScript code 13
+Symbol 14
+JavaScript code with scope 15
+32-bit integer 16
+Timestamp 17
+64-bit integer 18
+Min key 255
+Max key 127
+
+语法：
+db.coll.group(
+{ 
+    cond: {filed： conditions}
+    , key: {filed: true}
+    , initial: {count: 0, total_time:0}
+    , reduce: function(doc, out){ }
+    , finalize: function(out){}
+    } 
+);
+参数说明：
+Key： 对那个字段进行 Group
+Cond： 查询条件
+Initial： 初始化 group 计数器
+Reduce： 通常做统计操作
+Finalize： 通常都统计结果进行进一步操作，例如求平均值
+Keyf： 用一个函数来返回一个替代 KEY 的值
